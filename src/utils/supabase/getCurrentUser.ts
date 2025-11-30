48f544bb43c1d0bada67e415c0f99fef42b5c68e
Ignace928 <edwardoignace928@gmail.com>
2025-11-30 22:23:58 +0300

    Finition du service tache

--- src/utils/supabase/getCurrentUser.ts
+++ src/utils/supabase/getCurrentUser.ts
@@ -1,36 +1,36 @@
 "use server";
 
 import prisma from "@/src/lib/prisma";
-// import { createClient } from "./server";
-
-
-// export async function getCurrentUser() {
-//   const supabase = await createClient();
-//   const { data: { user } } = await supabase.auth.getUser();
-
-//   if (!user) return null;
-//   const emaiUser = user.email
-//   let dbUser = await prisma.user.findUnique({ where: { id: user.id } });
-
-//   if (!dbUser) {
-//     dbUser = await prisma.user.create({ data: { id: user.id } });
-//   }
-
-//   return {
-//         id : dbUser.id,
-//         email: emaiUser || "default",
-//       };
-// }
-
+import { createClient } from "./server";
 
 
 export async function getCurrentUser() {
-  const dbUser = await prisma.user.findUnique({
-    where:{id:"user3"}
-  })
-  if(!dbUser) return {id:"null", email:"no"}
+  const supabase = await createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+
+  if (!user) return null;
+  const emaiUser = user.email
+  let dbUser = await prisma.user.findUnique({ where: { id: user.id } });
+
+  if (!dbUser) {
+    dbUser = await prisma.user.create({ data: { id: user.id } });
+  }
+
   return {
-    id : dbUser.id,
-    email: dbUser.id,
-  };
+        id : dbUser.id,
+        email: emaiUser || "default",
+      };
 }
+
+
+
+// export async function getCurrentUser() {
+//   const dbUser = await prisma.user.findUnique({
+//     where:{id:"user3"}
+//   })
+//   if(!dbUser) return {id:"null", email:"no"}
+//   return {
+//     id : dbUser.id,
+//     email: dbUser.id,
+//   };
+// }
