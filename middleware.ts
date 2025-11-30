4a9974b47a4b717c7a0510c371a353e75e6b4b3d
Ignace928 <edwardoignace928@gmail.com>
2025-11-30 22:36:21 +0300

    Fixer le middleware

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,8 @@
-import { NextResponse, type NextRequest } from "next/server"
+import { type NextRequest } from "next/server"
 import { updateSession } from "@/src/utils/supabase/middleware"
 
 export async function middleware(req: NextRequest) {
     const res = await updateSession(req);
-
-    // Si pas de session → redirige côté serveur (SEO friendly)
-    const pathname = req.nextUrl.pathname;
-
-    const isPublicPage =
-      pathname.startsWith("/login") ||
-      pathname.startsWith("/register") ||
-      pathname.startsWith("/public");
-
-    if (!isPublicPage && !req.cookies.get("sb-access-token")) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-
     return res;
 }
 
